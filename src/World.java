d0b419dab8886439b8fe46d3cc6ff636fc992e4f
Antoshechkaa <kashtelyan.anton@gmail.com>
2022-07-10 17:59:28 +0400

    3 stage

--- src/World.java
+++ src/World.java
@@ -1,7 +1,12 @@
 public class World {
 
     public static void main(String[] args)   {
-        System.out.print("at start ");
-        System.out.println("Hello World");
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
     }
 }
